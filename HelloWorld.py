19e3e3ea13954361e4d6de4c72f99c473d8d08a5
Bradley Hayes <bhaye003@ucr.edu>
2018-04-22 04:42:07 +0000

    Relocated old C++ files

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+import random
+import sys
+import os
+
+
+glist = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
+
+random_num = random.randrange(0, 100)
+
+while(random_num != 15, 16):
+    print(random_num)
+    random_num = random.randrange(0,100)
